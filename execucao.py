d7f5a03571a925c74d66e0caab415477600eed97
Miguel-casarin <casarindasilvamiguel@gmail.com>
2024-10-05 22:33:50 -0300

    novo saparado

--- execucao.py
+++ execucao.py
@@ -2,23 +2,17 @@ from conexao import conectar
 
 def execute(connection, sql):
    
-    cursor = connection.cursor()  # 10
-    cursor.execute(sql)  # 11S
+    cursor = connection.cursor()  
+    cursor.execute(sql)  
     results = cursor.fetchall()
-
     return results
   
+conn = conectar()  
+select_livros = "SELECT * FROM livros;"  
 
-conn = conectar()  # 17
+livros = execute(conn, select_livros)  
+for i in livros:  
+    print(i)  
 
-# Comando SQL corrigido
-select_livros = "SELECT * FROM livros;"  # 18 (Corrigido para duas aspas)
-
-# Executa a consulta e imprime os resultados
-livros = execute(conn, select_livros)  # 19
-for i in livros:  # 20
-    print(i)  # 21
-
-# Fecha a conexão
-conn.close()  # 22
-print("Conexão encerrada.")  # 23
+conn.close()  
+print("Conexão encerrada.")  

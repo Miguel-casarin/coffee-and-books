413fae29dffb270878aeca8d669ceba37d6bb96e
Miguel-casarin <casarindasilvamiguel@gmail.com>
2024-10-05 22:36:28 -0300

    bug corrigido

--- conexao.py
+++ conexao.py
@@ -10,28 +10,6 @@ def conectar():
         host="localhost",           # Substitua pelo endereço do host do banco de dados
         port="5432"                 # Porta padrão do PostgreSQL
     )
-    print("Conexão bem-sucedida ao banco de dados!")  # 8
+    #print("Conexão bem-sucedida ao banco de dados!")  # 8
     return conn  # 9  (Corrigido para retornar a variável conn)
 
-def execute(connection, sql):
-   
-    cursor = connection.cursor()  # 10
-    cursor.execute(sql)  # 11S
-    results = cursor.fetchall()
-
-    return results
-  
-
-conn = conectar()  # 17
-
-# Comando SQL corrigido
-select_livros = "SELECT * FROM livros;"  # 18 (Corrigido para duas aspas)
-
-# Executa a consulta e imprime os resultados
-livros = execute(conn, select_livros)  # 19
-for i in livros:  # 20
-    print(i)  # 21
-
-# Fecha a conexão
-conn.close()  # 22
-print("Conexão encerrada.")  # 23
